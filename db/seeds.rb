68a6d1e0080026bb7b13103b7af5778bf41037cf
Aziret Alisher <aziretalisher@gmail.com>
2023-04-24 01:58:20 +0100

    Add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,22 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+include FactoryBot::Syntax::Methods
+
+['open', 'in progress', 'done', 'to do', 'under review', 'approved', 'cancelled'].each do |status_name|
+  Status.create(name: status_name) if Status.where(name: status_name).blank?
+end
+
+5.times do
+  create(:project)
+end
+
+statuses = Status.all
+
+Project.all.each do |project|
+  statuses.each do |status|
+    project.project_histories.create(actionable: status)
+    comment = create(:comment)
+    project.project_histories.create(actionable: comment)
+  end
+end
